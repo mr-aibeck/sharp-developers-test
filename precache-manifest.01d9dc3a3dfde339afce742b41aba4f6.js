7da0c7da9caf8b4c1a68017f4cf5fc1e71540f45
Aibeck <mr.aibeck@gmail.com>
2020-05-27 17:53:45 +0300

    Updates

---
+++ precache-manifest.01d9dc3a3dfde339afce742b41aba4f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0b49fd72ece0a4a6335136aa597670",
+    "url": "/sharp-developers-test/index.html"
+  },
+  {
+    "revision": "e6f5b8e57e947d0b9f20",
+    "url": "/sharp-developers-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/sharp-developers-test/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sharp-developers-test/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f5b8e57e947d0b9f20",
+    "url": "/sharp-developers-test/static/js/main.9128821b.chunk.js"
+  },
+  {
+    "revision": "eefb5af1498fd8fc4308",
+    "url": "/sharp-developers-test/static/js/runtime-main.55bfb313.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sharp-developers-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
